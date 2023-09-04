e7a1836ee7ad8c1bf76b62cc99de5ba75d9d6742
svesTECHEVENT <144025248+svesTECHEVENT@users.noreply.github.com>
2023-09-04 20:29:45 +0530

    Add files via upload

--- src/componetes/RegisterLinks.js
+++ src/componetes/RegisterLinks.js
@@ -15,7 +15,8 @@ export const RegisterLinks=()=>{
                 <h3>Event1</h3>
              {arrow?<div onClick={()=>arrowFun()}><span class="material-symbols-outlined">
 expand_less
-</span></div>:<div onClick={()=>arrowFun()}><span class="material-symbols-outlined">keyboard_arrow_down</span></div>}
+</span> <a href="https://docs.google.com/forms/d/e/1FAIpQLSeYMTXDyDF6TIqnss8MC5G6AoTU1-H1q0qHZEsJDEFnuZ1Ywg/viewform?usp=sf_link">clk</a></div>:<div onClick={()=>arrowFun()}>
+<span class="material-symbols-outlined">keyboard_arrow_down</span></div>}
            </div>
         </div>
     </div>
