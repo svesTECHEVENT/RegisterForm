dcd499c7b070b077817f6762a13d962629dc652b
svesTECHEVENT <144025248+svesTECHEVENT@users.noreply.github.com>
2023-09-04 22:55:21 +0530

    Add files via upload

--- src/componetes/Header.js
+++ src/componetes/Header.js
@@ -3,7 +3,7 @@ export const Header=()=>{
         <>
                     <img src="https://srivasaviengg.ac.in/imagesbalu/vasavisvec_logo.png"></img>
                 <div className="title header_flex">
-                <h1>TechEvent 2k23</h1>
+                <h2>TechEvent 2k23</h2>
                     <h2> Registration form</h2>
                 </div>
                   
